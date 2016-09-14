0fb5d9509b7037174bf185f139c4e5931df2c31c
Joseph Daniels <joedaniels29@me.com>
2016-09-13 20:21:21 -0400

    that script was easy.... too easy

--- assets/image_make.rb
+++ assets/image_make.rb
@@ -3,18 +3,13 @@ require 'ERB'
 require 'mini_magick'
 require 'fileutils'
 
-install = true
-json = JSON.parse(File.open('app/payload.json', 'rb').read)
-projects = json.reject { |x, v| x == 'skills' }.values.flatten
-
 DEFAULT_CARD_HEIGHT=215
 DEFAULT_CARD_WIDTH=400
 ICON_WIDTH = 200
 LOGO_WIDTH = ICON_WIDTH * 2
 
-IMAGE_ASSET_LOCATION = '/Users/Joe/Projects/Mine/ImgAssets'
-IMAGE_ASSET_OUTPUT_LOCATION = File.join(IMAGE_ASSET_LOCATION, 'output')
-IMAGES_LOCATION = './public/assets/images'
+IMAGE_ASSET_LOCATION = './originals'
+IMAGE_ASSET_OUTPUT_LOCATION = File.join(".", 'computed')
 
 def base(src)
   File.basename(src, File.extname(src))
@@ -30,27 +25,17 @@ def opacify(image)
   image.alpha 'Set'
 end
 
-def render(img, output_dir, type, width, height, dpi=1, fit)
-  img['src'] ||= img['id']
-  img["src"] = File.basename( Dir[File.join(IMAGE_ASSET_LOCATION, type, "*")].select { |f| f[/#{img['src']}/] }.first ) if File.extname(img['src']).empty? rescue throw "OMG the file for #{img["src"]} is missing!"
-
-  image = MiniMagick::Image.open(File.join(IMAGE_ASSET_LOCATION, type, img['src']))
-  image.path
-  size_string = "#{(img['card-size']||1)*dpi*width}x#{dpi*height}"
-  # if File.extname(img['src']) != ".svg"
-  # opacify image
-
-  # opacify image
-  # else
-  # end
+def render(img, output_dir, width, height, dpi=1, fit)
+  image = MiniMagick::Image.open(img)
+  size_string = "#{(width||1)*dpi}x#{dpi*height}"
   image.format("png") do |cmd|
-    opacify cmd
+    # opacify cmd
     cmd.scale(size_string+fit)
     cmd.gravity 'center'
-    cmd.density 300
-    if offset = img['offset']
-      cmd.crop "+#{dpi*offset['width']}+#{dpi*offset['height']}"
-    end
+    # cmd.density 300
+    # if offset = img['offset']
+    #   cmd.crop "+#{dpi*offset['width']}+#{dpi*offset['height']}"
+    # end
     cmd.extent size_string
   end
 
@@ -60,51 +45,13 @@ def render(img, output_dir, type, width, height, dpi=1, fit)
 end
 
 HASH = {
-    background: {width: DEFAULT_CARD_WIDTH, height: DEFAULT_CARD_HEIGHT, fit: "^"},
-    logo: {width: LOGO_WIDTH, height: ICON_WIDTH},
-    icon: {width: ICON_WIDTH, height: ICON_WIDTH},
-    screenshot: {width: DEFAULT_CARD_WIDTH, height: DEFAULT_CARD_HEIGHT}
+    clock: {width: DEFAULT_CARD_WIDTH, height: DEFAULT_CARD_HEIGHT },#fit: "^"
+    display: {width: LOGO_WIDTH, height: ICON_WIDTH},
 }
 types = HASH
 
+
 # images
-projects.each do |p|
-  types.keys.each do |type|
-    if img = p[type.to_s]
-      if img.kind_of?(Hash) && img['src']
-        output_dir = File.join(IMAGE_ASSET_OUTPUT_LOCATION, type.to_s)
-        `mkdir -p #{output_dir}`
-        render(img, output_dir, type.to_s, types[type][:width], types[type][:height], 1, types[type][:fit] || "")
-        render(img, output_dir, type.to_s, types[type][:width], types[type][:height], 2, types[type][:fit] || "")
-      end
-    end
-  end
-end
-
-output_dir = File.join(IMAGE_ASSET_OUTPUT_LOCATION, "skills")
-icons_dir = File.join(output_dir, "icons")
-logos_dir = File.join(output_dir, "logos")
-FileUtils::mkdir_p [icons_dir, logos_dir]
-json["skills"].each do |skill|
-
-  if skill["icon"]
-    render(skill, icons_dir, 'skills/icon', ICON_WIDTH, ICON_WIDTH, 1, "")
-  elsif skill['logo']
-    render(skill, logos_dir, 'skills/logo', LOGO_WIDTH, ICON_WIDTH, 1, "")
-  end
-end
-
-
-if install
-  FileUtils::mkdir_p IMAGES_LOCATION
-  FileUtils::cp_r "#{IMAGE_ASSET_OUTPUT_LOCATION}/.", IMAGES_LOCATION
-end
-
-
-# scss
-@grid_item_img = projects.map { |project| src = (project.dig('background', 'src') rescue nil); src && !src.strip.empty? ? "'#{asset_relative("background", src)}'" : 'none' }.join(', ')
-@grid_item_colors = projects.map { |project| color = (project.dig('color', 'primary') rescue project.dig('color')); color && !color.strip.empty? ? color : '$default' }.join(', ')
-b = binding
-File.open('app/styles/dynamic_content.scss', 'w') do |f|
-  f.write(ERB.new(File.read('app/styles/dynamic_content.scss.erb')).result(b))
+Dir[IMAGE_ASSET_LOCATION + "/*"].each do |p|
+    render(img, IMAGE_ASSET_OUTPUT_LOCATION, types["clock"][:width], types[type][:height], 1, types[type][:fit] || "")
 end
