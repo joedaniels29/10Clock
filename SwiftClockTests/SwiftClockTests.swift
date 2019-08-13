6c90b61f515f3e3b7a13ad14c8cd8ebb79943859
Tomáš Sýkora <sykora@altamira.sk>
2019-08-13 10:11:09 +0200

    Migration to Swift 5

--- SwiftClockTests/SwiftClockTests.swift
+++ SwiftClockTests/SwiftClockTests.swift
@@ -32,15 +32,15 @@ class SwiftClockTests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let app = XCUIApplication()
-        let element = XCUIApplication().children(matching: .Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).elementBoundByIndex(1)
+        let element = XCUIApplication().children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 1)
         element.tap()
         element.tap()
         element.tap()
         element.tap()
         element.tap()
-        let fromCoordinate = app.coordinateWithNormalizedOffset(CGVector(dx: 0, dy: 10))
-        let toCoordinate = app.coordinateWithNormalizedOffset(CGVector(dx: 0, dy: 20))
-        fromCoordinate.pressForDuration(0, thenDragToCoordinate: toCoordinate)
+        let fromCoordinate = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 10))
+        let toCoordinate = app.coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 20))
+        fromCoordinate.press(forDuration: 0, thenDragTo: toCoordinate)
         
     }
     
